1a41a46ae4e83cc8c44cf0911c29e6bfc0604c45
sleeplesseditor <josh@bryson.me.uk>
2018-03-08 14:07:40 +0000

    Improved app structure

--- src/index.js
+++ src/index.js
@@ -3,40 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'react-bootstrap';
 
-import Box from './components/Box';
 import Buttons from './components/Buttons';
+import Grid from './components/Grid';
 
 function arrayClone(arr) {
     return JSON.parse(JSON.stringify(arr));
 }
-
-const Grid = props => {
-    const width = props.cols * 14;
-    let boxClass = "";
-    const rowsArr = props.gridFull.map((rowArr, rowIdx) => 
-        rowArr.map((item, colIdx) => {
-            const boxId = `${rowIdx}_${colIdx}`;
-
-            boxClass = props.gridFull[rowIdx][colIdx] ? "box on" : "box off";
-            return (
-                <Box 
-                    boxClass={boxClass}
-                    key={boxId}
-                    boxId={boxId}
-                    row={rowIdx}
-                    col={colIdx}
-                    selectBox={props.selectBox}
-                />
-            );
-        })
-    );
-
-    return (
-        <div className="grid" style={{ width }}>
-            {rowsArr}
-        </div>
-    );
-};
 
 class Main extends Component {
     constructor() {
